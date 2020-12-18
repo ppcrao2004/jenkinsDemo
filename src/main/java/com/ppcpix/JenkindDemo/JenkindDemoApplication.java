92f1afe1ef9e2c133a5bf8c197ea020633bea469
Chandu pakala <ppcrao2004@gmail.com>
2020-12-17 22:27:15 -0600

    First Commit
    First Commit

---
+++ src/main/java/com/ppcpix/JenkindDemo/JenkindDemoApplication.java
@@ -0,0 +1,13 @@
+package com.ppcpix.JenkindDemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JenkindDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JenkindDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ppcpix/JenkindDemo/JenkindDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ppcpix.JenkindDemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JenkindDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
